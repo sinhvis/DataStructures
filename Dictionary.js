844322036079ba335cf20df22bc3f75be02f9de0
sinhvis <sinhvis@gmail.com>
2016-05-03 17:51:48 -0700

    Added Dictionary and driver

---
+++ Dictionary.js
@@ -0,0 +1,46 @@
+// Dictionary stores data as key-value pairs
+// Object class is designed to operate the dictionary
+// Basis for Dictionary class is Array class rather than Object
+// because need to be able to sort keys of a dictionary, and 
+// JavaScript can't sort the properties of an Object
+// However, everything in JavaScript is an object, so array is an object.
+// Use with dictionaryDriver.js
+
+function Dictionary() {
+	this.datastore = new Array() ;
+	this.add = add ;
+	this.find = find ;
+	this.remove = remove ;
+	this.showAll = showAll ;
+}
+
+// add() - takes key and value. 
+// key is the index for the value element.
+function add(key, value) {
+	this.datastore[key] = value ;
+}
+
+// find() - takes key and returns value associated with it
+function find(key) {
+	return this.datastore[key] ;
+}
+
+// remove() - uses a built-in function called delete
+// function is part of Object, and takes a reference to key
+function remove(key) {
+	delete this.datastore[key] ;
+}
+
+// showAll() - keys(), when called with an object, returns all keys 
+// stored in the object
+//function showAll() {
+//	for each (var key in Object.keys(this.datastore)) {
+//		print(key + " -> " + this.datastore[key]) ;
+//	}
+//}
+
+function showAll() {
+	for(var key in this.datastore) {
+		print(key + " ->  " + this.datastore[key]);
+	}
+}
---
+++ test.js
@@ -0,0 +1,20 @@
+//var datastore = [] ;
+//
+//datastore['vishal'] = 'fool' ;
+//print(datastore['vishal']) ;
+//
+//datastore['web'] = 'JavaScript' ;
+//
+var sum = 0 ;
+// var obj = {prop1: 5, prop2: 13, prop3: 8} ;
+var arr = [1, 2, 3, 4, 5] ;
+
+//for each (var item in obj) {
+//	sum += item ;
+//}
+
+for each (var item in arr) {
+	sum += item ;
+}
+
+console.log(sum) ;
